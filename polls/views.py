c810f7d7667ecb1f8e3a252d600e69865c3a5a65
welitis <943446906@qq.com>
2019-02-25 22:23:14 +0800

    polls not class view

---
+++ polls/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404,redirect
+from django.http import HttpResponse
+from .models import Question, Choice
+from django.urls import reverse
+
+
+# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', locals())
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question":question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question":question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST.get('choice'))
+    except:
+        return render(request, "polls/detail.html", {
+            "question":question,
+            "error_message":"You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect(reverse("polls:results", args=(question.id,)))
+
